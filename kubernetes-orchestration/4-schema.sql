51b70dd383b9772e3c2220babd8bcce5cb061b28
mdrdani <sudokill3004@gmail.com>
2023-02-28 09:10:31 +0700

    reconfig Pod volume service and schema sql

--- kubernetes-orchestration/4-schema.sql
+++ kubernetes-orchestration/4-schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS hello_world;
 USE hello_world;
 
 CREATE TABLE IF NOT EXISTS users (
-     if int NOT NULL AUTO_INCREMENT,
+     id int NOT NULL AUTO_INCREMENT,
      first_name VARCHAR(255) NOT NULL,
      last_name VARCHAR(255),
      birth DATETIME,
      PRIMARY KEY (id)
-)
+);
